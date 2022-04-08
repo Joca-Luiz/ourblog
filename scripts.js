4bcd15496c38aba29b43790a615c7d998843806b
jocaluiz <jlpradoneiva@gmail.com>
2022-04-08 18:21:12 -0300

    Version 1.0.0

---
+++ scripts.js
@@ -0,0 +1,63 @@
+const $sections = document.querySelectorAll('.sections section');
+
+const $about = document.querySelector('.about');
+const $closeButton = document.querySelector('.about img');
+$closeButton.addEventListener('click', e => e.target.parentNode.style.display = "none");
+
+const $title = document.getElementById('title');
+const $text = document.getElementById('text')
+const $author = document.getElementById('author');
+
+const titles = {
+    0: "Switzerland",
+    1: "Leysin American School",
+    2: "Budgets and Burocracit"
+}
+
+const texts = {
+    0: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla metus, lobortis in sollicitudin lobortis, interdum eu orci. Sed eget sollicitudin tellus, quis bibendum mauris. Integer sed est scelerisque, faucibus est sed, molestie neque. Vivamus id pharetra ipsum. In nec risus quis libero pharetra aliquam ut a elit. Praesent ut massa eros. Phasellus ac lorem nec arcu ornare lacinia. Integer rhoncus aliquam dui, sit amet porttitor ipsum ornare eu. Phasellus feugiat tortor turpis. Donec pulvinar, nisi a auctor sagittis, enim felis maximus elit, in congue nisl leo vel massa. Quisque vitae iaculis elit, eu varius nibh. Sed turpis velit, ullamcorper id mauris eget, faucibus eleifend dolor. Sed molestie orci quis fermentum interdum. Duis auctor bibendum odio. Mauris ut bibendum ex. Suspendisse placerat, ex non fermentum tincidunt, sapien odio blandit erat, vitae euismod tellus ex dignissim ante.
+
+    Etiam in diam lectus. Donec sagittis tortor et faucibus dapibus. Vestibulum suscipit ornare metus, non imperdiet neque sagittis ac. Cras pretium nisl eget convallis imperdiet. Sed id ex malesuada lacus dapibus tristique ac eget urna. Duis id mauris dui. Maecenas in volutpat libero. Pellentesque ultricies a metus ac varius. Donec massa mauris, fermentum id dui eu, blandit congue ex. Nam interdum dignissim viverra. Mauris iaculis a lorem ac hendrerit. Aenean tincidunt rhoncus velit sit amet consequat. Ut at turpis congue, commodo dolor nec, interdum dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed maximus dui, ac ultricies augue.
+    
+    Morbi interdum a dolor non ullamcorper. Donec ac sem eros. Curabitur feugiat est fringilla sem maximus, vel fermentum felis mollis. Etiam pellentesque ultricies auctor. Mauris ligula lectus, commodo nec iaculis at, vestibulum nec massa. Donec enim diam, blandit ut metus quis, efficitur ultricies lectus. Nam sed laoreet tortor. Aenean fermentum sed sem eget facilisis. Donec eleifend rhoncus ante, vitae consequat lectus porta quis.
+    
+    Nulla non aliquam purus. Nam iaculis consectetur mauris, euismod tempus risus lobortis sit amet. In libero libero, semper vitae magna a, suscipit pretium tortor. Vestibulum massa ante, venenatis quis nunc bibendum, lacinia dictum sapien. Ut in varius nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus pharetra sapien vitae tellus pharetra pulvinar. Donec et posuere risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque pharetra sed sem nec tempus. Curabitur eleifend semper diam at lacinia. Sed magna diam, eleifend sit amet dui non, aliquet lobortis tortor. Maecenas aliquam posuere est sed lobortis.
+    
+    Donec eget urna at neque iaculis imperdiet at aliquam urna. In leo neque, blandit sed metus ac, gravida dapibus nisl. Vestibulum interdum volutpat ante, pulvinar sagittis lectus aliquet id. Aliquam consequat dui eget enim lobortis, sit amet maximus enim gravida. In eu semper leo. Nullam a euismod dolor. Aliquam interdum erat justo, sit amet ultricies enim sagittis sed.`,
+    1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla metus, lobortis in sollicitudin lobortis, interdum eu orci. Sed eget sollicitudin tellus, quis bibendum mauris. Integer sed est scelerisque, faucibus est sed, molestie neque. Vivamus id pharetra ipsum. In nec risus quis libero pharetra aliquam ut a elit. Praesent ut massa eros. Phasellus ac lorem nec arcu ornare lacinia. Integer rhoncus aliquam dui, sit amet porttitor ipsum ornare eu. Phasellus feugiat tortor turpis. Donec pulvinar, nisi a auctor sagittis, enim felis maximus elit, in congue nisl leo vel massa. Quisque vitae iaculis elit, eu varius nibh. Sed turpis velit, ullamcorper id mauris eget, faucibus eleifend dolor. Sed molestie orci quis fermentum interdum. Duis auctor bibendum odio. Mauris ut bibendum ex. Suspendisse placerat, ex non fermentum tincidunt, sapien odio blandit erat, vitae euismod tellus ex dignissim ante.
+
+    Etiam in diam lectus. Donec sagittis tortor et faucibus dapibus. Vestibulum suscipit ornare metus, non imperdiet neque sagittis ac. Cras pretium nisl eget convallis imperdiet. Sed id ex malesuada lacus dapibus tristique ac eget urna. Duis id mauris dui. Maecenas in volutpat libero. Pellentesque ultricies a metus ac varius. Donec massa mauris, fermentum id dui eu, blandit congue ex. Nam interdum dignissim viverra. Mauris iaculis a lorem ac hendrerit. Aenean tincidunt rhoncus velit sit amet consequat. Ut at turpis congue, commodo dolor nec, interdum dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed maximus dui, ac ultricies augue.
+    
+    Morbi interdum a dolor non ullamcorper. Donec ac sem eros. Curabitur feugiat est fringilla sem maximus, vel fermentum felis mollis. Etiam pellentesque ultricies auctor. Mauris ligula lectus, commodo nec iaculis at, vestibulum nec massa. Donec enim diam, blandit ut metus quis, efficitur ultricies lectus. Nam sed laoreet tortor. Aenean fermentum sed sem eget facilisis. Donec eleifend rhoncus ante, vitae consequat lectus porta quis.
+    
+    Nulla non aliquam purus. Nam iaculis consectetur mauris, euismod tempus risus lobortis sit amet. In libero libero, semper vitae magna a, suscipit pretium tortor. Vestibulum massa ante, venenatis quis nunc bibendum, lacinia dictum sapien. Ut in varius nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus pharetra sapien vitae tellus pharetra pulvinar. Donec et posuere risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque pharetra sed sem nec tempus. Curabitur eleifend semper diam at lacinia. Sed magna diam, eleifend sit amet dui non, aliquet lobortis tortor. Maecenas aliquam posuere est sed lobortis.
+    
+    Donec eget urna at neque iaculis imperdiet at aliquam urna. In leo neque, blandit sed metus ac, gravida dapibus nisl. Vestibulum interdum volutpat ante, pulvinar sagittis lectus aliquet id. Aliquam consequat dui eget enim lobortis, sit amet maximus enim gravida. In eu semper leo. Nullam a euismod dolor. Aliquam interdum erat justo, sit amet ultricies enim sagittis sed.`,
+    2: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla metus, lobortis in sollicitudin lobortis, interdum eu orci. Sed eget sollicitudin tellus, quis bibendum mauris. Integer sed est scelerisque, faucibus est sed, molestie neque. Vivamus id pharetra ipsum. In nec risus quis libero pharetra aliquam ut a elit. Praesent ut massa eros. Phasellus ac lorem nec arcu ornare lacinia. Integer rhoncus aliquam dui, sit amet porttitor ipsum ornare eu. Phasellus feugiat tortor turpis. Donec pulvinar, nisi a auctor sagittis, enim felis maximus elit, in congue nisl leo vel massa. Quisque vitae iaculis elit, eu varius nibh. Sed turpis velit, ullamcorper id mauris eget, faucibus eleifend dolor. Sed molestie orci quis fermentum interdum. Duis auctor bibendum odio. Mauris ut bibendum ex. Suspendisse placerat, ex non fermentum tincidunt, sapien odio blandit erat, vitae euismod tellus ex dignissim ante.
+
+    Etiam in diam lectus. Donec sagittis tortor et faucibus dapibus. Vestibulum suscipit ornare metus, non imperdiet neque sagittis ac. Cras pretium nisl eget convallis imperdiet. Sed id ex malesuada lacus dapibus tristique ac eget urna. Duis id mauris dui. Maecenas in volutpat libero. Pellentesque ultricies a metus ac varius. Donec massa mauris, fermentum id dui eu, blandit congue ex. Nam interdum dignissim viverra. Mauris iaculis a lorem ac hendrerit. Aenean tincidunt rhoncus velit sit amet consequat. Ut at turpis congue, commodo dolor nec, interdum dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed maximus dui, ac ultricies augue.
+    
+    Morbi interdum a dolor non ullamcorper. Donec ac sem eros. Curabitur feugiat est fringilla sem maximus, vel fermentum felis mollis. Etiam pellentesque ultricies auctor. Mauris ligula lectus, commodo nec iaculis at, vestibulum nec massa. Donec enim diam, blandit ut metus quis, efficitur ultricies lectus. Nam sed laoreet tortor. Aenean fermentum sed sem eget facilisis. Donec eleifend rhoncus ante, vitae consequat lectus porta quis.
+    
+    Nulla non aliquam purus. Nam iaculis consectetur mauris, euismod tempus risus lobortis sit amet. In libero libero, semper vitae magna a, suscipit pretium tortor. Vestibulum massa ante, venenatis quis nunc bibendum, lacinia dictum sapien. Ut in varius nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus pharetra sapien vitae tellus pharetra pulvinar. Donec et posuere risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque pharetra sed sem nec tempus. Curabitur eleifend semper diam at lacinia. Sed magna diam, eleifend sit amet dui non, aliquet lobortis tortor. Maecenas aliquam posuere est sed lobortis.
+    
+    Donec eget urna at neque iaculis imperdiet at aliquam urna. In leo neque, blandit sed metus ac, gravida dapibus nisl. Vestibulum interdum volutpat ante, pulvinar sagittis lectus aliquet id. Aliquam consequat dui eget enim lobortis, sit amet maximus enim gravida. In eu semper leo. Nullam a euismod dolor. Aliquam interdum erat justo, sit amet ultricies enim sagittis sed.`
+}
+
+const authors = {
+    0: "Isabella",
+    1: "Isabelly",
+    2: "Sofia"
+}
+
+$sections.forEach((el, i) => el.addEventListener('click', () => {
+    if ($about.style.display == "none") {
+        $about.style.display = "block"
+    }
+
+    window.location.href = "#more";
+
+    $title.innerHTML = titles[i];
+    $text.innerHTML = texts[i];
+    $author.innerHTML = authors[i];
+}));
